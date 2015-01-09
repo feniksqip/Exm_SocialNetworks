b9ceb47763df1bfb5372cdc0e51810d65e6d7cc0
Admin <admin@Mac-Admin.local>
2015-01-09 22:53:38 +0300

    Create.

---
+++ Exm_SocialNetworks/Exm_SocialNetworks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exm_SocialNetworks
+//
+//  Created by Admin on 09.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Exm_SocialNetworks/Exm_SocialNetworksTests/Exm_SocialNetworksTests.m
@@ -0,0 +1,40 @@
+//
+//  Exm_SocialNetworksTests.m
+//  Exm_SocialNetworksTests
+//
+//  Created by Admin on 09.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Exm_SocialNetworksTests : XCTestCase
+
+@end
+
+@implementation Exm_SocialNetworksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
