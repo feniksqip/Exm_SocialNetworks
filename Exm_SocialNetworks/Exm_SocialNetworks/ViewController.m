241641cb7c9b326bdf6a0b4b02569fa337e19f66
Admin <admin@Mac-Admin.local>
2015-01-10 13:20:18 +0300

    Finish.

--- Exm_SocialNetworks/Exm_SocialNetworks/ViewController.m
+++ Exm_SocialNetworks/Exm_SocialNetworks/ViewController.m
@@ -30,20 +30,35 @@
         SLComposeViewController *composeCtrl = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
         NSString *text = @"Text for Twitter !";
         [composeCtrl setInitialText:text];
+        [composeCtrl addURL:[NSURL URLWithString:@"https://twitter.com/feniksqip"]];
+        [composeCtrl addImage:[UIImage imageNamed:@"Ship.jpg"]];
         [self presentViewController:composeCtrl animated:YES completion:nil];
     }
-    
-        
-    
- //   SLComposeViewController *socialComposeCtrl = [[SLComposeViewController alloc] init];
-//    if (socialComposeCtrl tw) {
-//        <#statements#>
-//    } else {
-//        <#statements#>
-//    }
-    
+    else {
+        //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=TWITTER"]];
+       // [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs://"]];
+        // For iOS 8 and above
+        NSURL *settingsURL = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+        [[UIApplication sharedApplication] openURL:settingsURL];
+    }
+
 }
 
 - (IBAction)facebookBtn:(id)sender {
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+        SLComposeViewController *composeCtrl = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        NSString *text = @"One, two, sri Facebook !";
+        [composeCtrl setInitialText:text];
+        [composeCtrl addURL:[NSURL URLWithString:@"https://twitter.com/feniksqip"]];
+        [composeCtrl addImage:[UIImage imageNamed:@"Ship.jpg"]];
+        [self presentViewController:composeCtrl animated:YES completion:nil];
+    }
+    else {
+        //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=FACEBOOK"]];
+        // [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs://"]];
+        // For iOS 8 and above
+        NSURL *settingsURL = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+        [[UIApplication sharedApplication] openURL:settingsURL];
+    }
 }
 @end
